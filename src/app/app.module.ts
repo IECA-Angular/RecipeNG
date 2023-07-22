fb64ee926ef0e6ce1e5f0e25237899d03840d288
Josué Caro <josuecaro@MacBook-Pro-de-Josue.local>
2023-07-22 11:48:42 -0600

    Loader

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoaderComponent } from './components/loader/loader.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    LoaderComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
